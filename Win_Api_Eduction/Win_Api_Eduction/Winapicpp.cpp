409e14802724bed1a9e1fb59becea19c01cb6007
DESKTOP-V10EKSP\user <coco1011@kakao.com>
2024-02-22 03:38:44 +0900

    for test

--- Win_Api_Eduction/Win_Api_Eduction/Winapicpp.cpp
+++ Win_Api_Eduction/Win_Api_Eduction/Winapicpp.cpp
@@ -2,21 +2,50 @@
 
 using namespace std;
 
-
 /* char 
 *  short
 *  int
 *  __int64 (long long)
 */
-int a;
-short mp;
+
+int cnt;
 
 int main(void)
 {
-	a = 306189112;
-	mp = a;
+	cnt = 5;
 
-	cout << mp;
+	//while (cnt < 5)
+	//{
+	//	cout << "Hello Wrold!" << endl;
+	//	cnt++;
+	//}
 
+
+	for (int i = 0; i < cnt; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << " ";
+		}	
+
+		for (int j = 2 * cnt - 1; j > 2 * i; j--)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	int num = 5;
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < i; j++) {
+			printf(" ");
+		}
+
+		for (int j = 2 * num - 1; j > 2 * i; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	
 	return 0;
 }
