42602510a22df699185d885161c27789ab524fcb
DESKTOP-V10EKSP\user <coco1011@kakao.com>
2024-02-28 01:44:21 +0900

    포인터 개념

--- Win_Api_Eduction/Win_Api_Eduction/소스.cpp
+++ Win_Api_Eduction/Win_Api_Eduction/소스.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
-int main()
+void AddHp(int hp, int value)
 {
 
+}
+
+
+int main()
+{
+	int hp = 100;
+
+	// [타입]*  [이름]
+	// - * = 포인터
+	// * 주소값을 담는 바구니
+	void* ptr;
+
+	AddHp(hp, 10);
 }
